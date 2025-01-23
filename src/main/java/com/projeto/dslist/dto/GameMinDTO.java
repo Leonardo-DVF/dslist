0e6b17bad8e6a08c1545aba1cfc4aa0f7c5f385b
Léo <leonardoveiga010@gmail.com>
2025-01-23 17:25:03 -0300

    Move games on list

--- src/main/java/com/projeto/dslist/dto/GameMinDTO.java
+++ src/main/java/com/projeto/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.projeto.dslist.dto;
 
 import com.projeto.dslist.entities.Game;
+import com.projeto.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 	
@@ -21,6 +22,14 @@ public class GameMinDTO {
 		shortDescription = entity.getShortDescription();
 	}
 
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getGameYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
+	}
+	
 	public Long getId() {
 		return id;
 	}
